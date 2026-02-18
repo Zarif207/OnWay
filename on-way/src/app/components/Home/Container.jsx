828d03f2a4124390b11ad82364233f5d7f7c103e
Minhaj Islam <infoglb.net@gmail.com>
2026-02-18 14:16:03 +0600

    checkout intiallize

--- on-way/src/app/components/Home/Container.jsx
+++ on-way/src/app/components/Home/Container.jsx
@@ -1,5 +1,6 @@
 export default function Container({ children, className = "" }) {
   return (
+    // Just for checkout
     <div className={`mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`}>
       {children}
     </div>

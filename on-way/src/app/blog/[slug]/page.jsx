2c98f919f12a453f18e93254b1a972eab3141a69
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-02-18 16:36:26 +0600

    [ADDED]: Custom json file added

---
+++ on-way/src/app/blog/[slug]/page.jsx
@@ -0,0 +1,9 @@
+
+export default function BlogDetails() {
+
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
---
+++ on-way/src/app/components/BlogCard.jsx
@@ -0,0 +1,8 @@
+
+export default function BlogCard({ blog }) {
+    return (
+        <div>
+            hello
+        </div>
+    );
+}
